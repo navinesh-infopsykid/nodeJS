920380f8887e83841ee48dbb808e164260e7305a
Navineshraj <navineshraj@trudosys.com>
2026-02-19 15:46:52 +0530

    server JS related info

--- http-server/simple-server.js
+++ http-server/simple-server.js
@@ -1,10 +1,28 @@
 const http = require('node:http');
+const fs= require('node:fs')
 
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{
-        "content-type":"text/plain"
+        "content-type":"text/html"
     });
-    res.end("Hello Node js Server");
+   // res.setHeader('Content-Type','text/html');
+   console.log(req.url);
+   let path ='../docs/';
+   if(req.url=='/home'||req.url=='/'){
+        path+= 'index.html';
+   }else if(req.url=='/about'||req.url=='/no'){
+        path+= 'about.html';
+   }else{
+    path+='default.html';
+   }
+    fs.readFile(path,(err, data)=>{
+        if(err){
+            console.log(err.message);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    })
     
 })
 
